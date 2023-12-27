e22510551ee41ec6dd7d7ee380b34780fc4d20c6
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-12-27 19:40:15 +0000

    hozon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,12 @@ function App() {
 
   return (
     <>
-      <h1>Test App</h1>
+      <main className="">
+        <h1>Test App</h1>
+        <p>以下にファイルを挿入し<br />モデルを動かしてみる</p>
+        <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+      </main>
+      
     </>
   )
 }
